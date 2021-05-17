8c17a266bb0e87c49e7f75c1bb45ee79168b5a51
Supun Priyashanka <79795518+supunpriyashanka@users.noreply.github.com>
2021-05-17 14:49:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ apiKey = "halyoa"
 # DON'T ABUSE THE API
 apiParams = f"&sort=0&category=0&page=0&adult=0&key={apiKey}&concurrent=0&provider_ids[]=32"
 botToken = 'your bot token'
-supportChatUrl = "https://t.me/itorrentsupportchat"
+supportChatUrl = "https://t.me/Network_hacker_bots"
 appUrl = 'https://play.google.com/store/apps/details?id=com.icodelife.itorrentsearch'
