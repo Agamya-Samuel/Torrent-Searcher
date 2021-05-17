c0294f47f0e936a60c422e1d74bb534ab89a2641
Supun Priyashanka <79795518+supunpriyashanka@users.noreply.github.com>
2021-05-17 14:43:34 +0530

    Update piratebay.py

--- piratebay.py
+++ piratebay.py
@@ -13,13 +13,13 @@ print("\nBot Started\n")
 
 @app.on_message(filters.command(['start']))
 async def start(_, message):
-    await message.reply_text("Hello I'm PirateBay Torrent Scraper Bot\nSend /help To Show Help Screen\nBot by @unkusr")
+    await message.reply_text("Hi, {}. I'm Torrent Sercher Bot\nSend /help To Show Help Screen\nBot by @NET_HACKER_BOTs_chat")
 
 
 
 @app.on_message(filters.command(['help']))
 async def help(_, message):
-    await message.reply_text("Example: /find titanic")
+    await message.reply_text("Example: /find Film Name")
 
 m = None
 i = 0
