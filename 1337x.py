0d583e21f56e800d0c44ed99e116f0d082f81a73
Supun Priyashanka <79795518+supunpriyashanka@users.noreply.github.com>
2021-05-17 14:32:06 +0530

    Update 1337x.py

--- 1337x.py
+++ 1337x.py
@@ -13,13 +13,13 @@ print("\nBot Started\n")
 
 @app.on_message(filters.command(['start']))
 async def start(_, message):
-    await message.reply_text("Hello I'm 1337x Torrent Scraper Bot\nSend /help To Show Help Screen\nBot by @unkusr")
+    await message.reply_text("Hello I'm Net Torrent Sercher Bot\nSend /help To Show Help Screen\nBot by @NET_HACKER_BOTs_chat")
 
 
 
 @app.on_message(filters.command(['help']))
 async def help(_, message):
-    await message.reply_text("Example: /find titanic")
+    await message.reply_text("Example: /find Film Name")
 
 m = None
 i = 0
